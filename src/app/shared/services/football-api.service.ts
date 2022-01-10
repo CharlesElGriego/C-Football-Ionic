5dd480b1857725671c4302624939391cfb4824ca
Charles <cnavarro3196@gmail.com>
2022-01-10 02:13:49 -0600

    feat: add transform squad to get info

--- src/app/shared/services/football-api.service.ts
+++ src/app/shared/services/football-api.service.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { sortByName } from '../helpers/array-helper';
+import { Player } from '../interfaces/player.interface';
 import { CompetionsResponse } from '../interfaces/responses/competitions-response.interface';
 import { TeamInformationResponse } from '../interfaces/responses/teamInformationResponse.interface';
 import { TeamsByCompetitionResponse } from '../interfaces/responses/teamsByCompetiton-response.interface';
+import { Team } from '../interfaces/team.interface';
 @Injectable({
   providedIn: 'root',
 })
 export class FootballApiService {
   private domainUrl = 'https://api.football-data.org/v2';
+  selectedTeam$ = new BehaviorSubject<Team>(null);
   constructor(private http: HttpClient) {}
 
   //#region   Public Methods
@@ -24,7 +27,12 @@ export class FootballApiService {
   }
 
   getTeamInfo(id: number): Observable<TeamInformationResponse> {
-    return this.http.get<TeamInformationResponse>(`${this.domainUrl}/teams/${id}`);
+    return this.http.get<TeamInformationResponse>(`${this.domainUrl}/teams/${id}`).pipe(
+      map((response) => {
+        response.squadByPosition = this.transformSquad(response.squad);
+        return response;
+      })
+    );
   }
 
   getCompetitions(): Observable<CompetionsResponse> {
@@ -34,6 +42,25 @@ export class FootballApiService {
         return response;
       })
     );
+  }
+
+  private transformSquad(squadPayload: Player[]) {
+    const playerPosition = new Map<string, Player[]>();
+    const squad = squadPayload;
+
+    squad.forEach((player) => {
+      if (playerPosition.has(player.position)) {
+        let players = [...playerPosition.get(player.position)];
+        players.push(player);
+        playerPosition.set(player.position, players.sort(sortByName));
+      } else {
+        let players = [];
+        players.push(player);
+        playerPosition.set(player.position, players.sort(sortByName));
+      }
+    });
+
+    return playerPosition;
   }
   //#endregion
 }
