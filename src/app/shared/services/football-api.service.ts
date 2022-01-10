369a58650fcd96ee7d37451d7fe24630cb01c886
Charles <cnavarro3196@gmail.com>
2022-01-09 22:59:02 -0600

    feat: add api endpoint with model

--- src/app/shared/services/football-api.service.ts
+++ src/app/shared/services/football-api.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Competition } from '../interfaces/competition.interface';
-import { Competitions } from '../interfaces/competitions.interface';
-
+import { map } from 'rxjs/operators';
+import { sortByName } from '../helpers/array-helper';
+import { CompetionsResponse } from '../interfaces/responses/competitions-response.interface';
+import { TeamInformationResponse } from '../interfaces/responses/teamInformationResponse.interface';
+import { TeamsByCompetitionResponse } from '../interfaces/responses/teamsByCompetiton-response.interface';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,12 +14,26 @@ export class FootballApiService {
   constructor(private http: HttpClient) {}
 
   //#region   Public Methods
-  getCompetition(id: string) {
-    return this.http.get<Competition>(`${this.domainUrl}/competitions`);
+  getTeamsByCompetition(id: number): Observable<TeamsByCompetitionResponse> {
+    return this.http.get<TeamsByCompetitionResponse>(`${this.domainUrl}/competitions/${id}/teams`).pipe(
+      map((response) => {
+        response.teams.sort(sortByName);
+        return response;
+      })
+    );
   }
 
-  getCompetitions(): Observable<Competitions> {
-    return this.http.get<Competitions>(`${this.domainUrl}/competitions`);
+  getTeamInfo(id: number): Observable<TeamInformationResponse> {
+    return this.http.get<TeamInformationResponse>(`${this.domainUrl}/teams/${id}`);
+  }
+
+  getCompetitions(): Observable<CompetionsResponse> {
+    return this.http.get<CompetionsResponse>(`${this.domainUrl}/competitions?plan=TIER_ONE`).pipe(
+      map((response) => {
+        response.competitions.sort(sortByName);
+        return response;
+      })
+    );
   }
   //#endregion
 }
