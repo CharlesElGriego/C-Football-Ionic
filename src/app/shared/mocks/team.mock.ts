dd9f86a815ffdfc01aff544080c1effc81f266e4
Charles <cnavarro3196@gmail.com>
2022-01-11 01:02:23 -0600

    feat(mock): add team

---
+++ src/app/shared/mocks/team.mock.ts
@@ -0,0 +1,23 @@
+import { Team } from '../interfaces/team.interface';
+
+export let teamMock: Team = {
+  id: 5,
+  area: {
+    id: 2088,
+    name: 'Germany',
+    countryCode: null,
+    ensignUrl: null,
+  },
+  name: 'FC Bayern München',
+  shortName: 'Bayern',
+  tla: 'FCB',
+  crestUrl: 'https://crests.football-data.org/5.svg',
+  address: 'Säbenerstr. 51 München 81547',
+  phone: '+49 (089) 699310',
+  website: 'http://www.fcbayern.de',
+  email: 'service-team@fcb.de',
+  founded: 1900,
+  clubColors: 'Red / White / Blue',
+  venue: 'Allianz Arena',
+  lastUpdated: '2021-12-16T18:52:55Z',
+};
