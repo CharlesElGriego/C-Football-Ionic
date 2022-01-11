dc29f43f121bebc0491d9a34e147b834cf1f539f
Charles <cnavarro3196@gmail.com>
2022-01-11 00:29:28 -0600

    feat(test): add team info

--- src/app/team-information/team-information.page.spec.ts
+++ src/app/team-information/team-information.page.spec.ts
@@ -2,16 +2,23 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
+import { BehaviorSubject, of } from 'rxjs';
+import { FootballApiService } from '../shared/services/football-api.service';
 import { TeamInformationPage } from './team-information.page';
 describe('TeamInformationPage', () => {
   let component: TeamInformationPage;
   let fixture: ComponentFixture<TeamInformationPage>;
+  let footballApiServiceStub = jasmine.createSpyObj('FootballApiService', ['getTeamInfo']);
+  footballApiServiceStub.getTeamInfo.and.returnValue(of({ id: 57, squad: [] }));
+  const selectedTeamStub$ = new BehaviorSubject<any>({});
+  footballApiServiceStub.selectedTeam$ = selectedTeamStub$;
 
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [TeamInformationPage],
         imports: [IonicModule.forRoot(), HttpClientTestingModule, RouterTestingModule],
+        providers: [{ provide: FootballApiService, useValue: footballApiServiceStub }],
       }).compileComponents();
 
       fixture = TestBed.createComponent(TeamInformationPage);
@@ -21,6 +28,12 @@ describe('TeamInformationPage', () => {
   );
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should get team info', () => {
+    fixture.detectChanges();
+    expect(component.teamInfo$.value.id).toBe(57);
   });
 });
